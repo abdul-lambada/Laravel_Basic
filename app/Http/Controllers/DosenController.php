b6f4b95d420ca77adbf2c05d639c089e15d7b91f
abdul-lambada <engineertekno@gmail.com>
2024-08-28 11:52:18 +0700

    Passing Data Controller ke View Laravel

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class DosenController extends Controller
 {
     public function index(){
-        return "Ini Adalah Controller Dosen Dengan method index";
+        // return "Ini Adalah Controller Dosen Dengan method index";
+        $nama = 'Dosen Name';
+
+        $sks = ['Logika Maematika', 'Kalkulus', 'Pem. Web'];
+        return view('biodata', ['nama' => $nama, 'matkul' => $sks]);
     }
 }
