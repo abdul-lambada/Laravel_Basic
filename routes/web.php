da537509a99fe2e405135bf56c7dde8207638d26
abdul-lambada <engineertekno@gmail.com>
2024-08-28 09:32:53 +0700

    Route Pertama

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Contoh Route Pertama
+Route::get('helo', function () {
+    return "Selamat Datang di Laravel";
+});
